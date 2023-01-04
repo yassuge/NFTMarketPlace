226f8b746d704863ed3ed7941baf6b96139b5c6d
Yassine <yassine@indexifi.io>
2023-01-04 16:39:41 +0000

    badge token unit test

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -3,6 +3,10 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
   networks: {
     localhost: {
       url: 'http://127.0.0.1:8545/'
